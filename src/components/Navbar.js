0524b5d12963970f94e5a147fa25813c4a80d72d
Awais <razhar5214@bths.edu>
2022-01-12 16:17:07 -0500

    issue #1 navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
+import '../index.css';
+import logo from "../zipcode-logo.jpg";
 
 export default function Navbar(){
     return(
-        <div>
-            
+        <div className="app--navbar">
+            <img className="app--logo" src={logo}/>
+            <h3 className="app--name">Zipcode Search</h3>
+            <h3 className="team--names">Awais. Shakhram. Rashad</h3>
         </div>
     )
 }
